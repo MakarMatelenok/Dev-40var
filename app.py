6cbb6b17fd695a6bfa015b58a91aa87ec482d2a5
MakarMatelenok <120810394+MakarMatelenok@users.noreply.github.com>
2025-03-17 21:43:45 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+delo_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
